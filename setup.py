974733bddfd8c5bd893dcb061eb5bbc5c5337a2c
Asankhaya Sharma <codelion@users.noreply.github.com>
2025-01-13 13:13:57 +0800

    Update setup.py
    Bump version for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = [
 
 setup(
     name="adaptive-classifier",
-    version="0.0.2",
+    version="0.0.3",
     author="codelion",
     author_email="codelion@okyasoft.com",
     description="A flexible, adaptive classification system for dynamic text classification",
